1fe3c605078c2a2ed07b452b0fd0c2491853bd8d
Dan <email@danielvanc.com>
2019-03-19 17:07:02 +0000

    Follow up amends for uses page and sanity upgrade

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -43,11 +43,12 @@ const LeftContent = styled(HTMLContent)`
     float: right;
     max-height: 460px;
     shape-outside: polygon(214px 26px, 97.37% 2.4%, 100.48% 0.32%, 419px 3px, 418px 571px, 102.01% 102.49%, 66.01% 86.08%, 7.92% 58%, 3.3% 55.14%, 3.53% 49.99%, 7.23% 46.33%, 79px 161px);
-    margin-right: -1.5em;
+    margin-right: -2em;
     padding: 1.313em 0 0.313em 1.313em;
     @media screen and (min-width: 75em) {
       height: 80%;
-      shape-outside: polygon(251px 71px, 91.51% 2.4%, 96.29% 0.53%, 419px 3px, 418px 571px, 100.75% 102.08%, 66.01% 86.08%, 7.92% 58%, 3.3% 55.14%, 3.53% 49.99%, 7.23% 46.33%, 92px 237px);
+      margin-left:-1.5em;
+      shape-outside: polygon(248px 51px, 100.12% 1.6%, 100.06% 99.8%, 66.01% 86.08%, 6.17% 59.53%, -0.87% 54.68%, -0.76% 49.95%, 2.84% 46.03%, 97px 213px);
       max-height: 630px;
     }
   }
@@ -64,13 +65,14 @@ const RightContent = styled(HTMLContent)`
     justify-self: center;
     float: left;
     shape-outside: polygon(-1px 7px, 4.21% 1.85%, 8.28% 4.47%, 53px 51px, 62.19% 29.71%, 89.29% 41.48%, 97.54% 46.9%, 96.89% 50.72%, 94.87% 55.14%, 8.14% 100.08%, 4px 678px);
-    margin-left: -1.5em;
+    margin-left: -2em;
     padding: 1.313em 1.5em 0.313em 0;
     shape-margin:0 10px 0 0;
     max-height: 460px;
     @media screen and (min-width: 75em) {
       shape-outside: polygon(0px 9px, 4.21% 1.85%, 9.1% 3.24%, 66px 51px, 65.90% 29.50%, 90.94% 41.89%, 98.78% 46.9%, 98.95% 50.72%, 97.34% 55.14%, 11.44% 99.26%, 4px 678px);
       height: 80%;
+      margin-left: -1.5em;
       max-height: 630px;
     }
   }
