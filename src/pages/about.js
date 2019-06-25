07c05dcc22e5325c5039062e18c3f4fc96788e8b
danielvanc <email@danielvanc.com>
2019-06-25 21:42:36 +0100

    [refs #005] Output data from sanity onto page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,23 +2,79 @@ import React from 'react'
 import styled from 'styled-components'
 import TitleAndMetas from '../components/Layout/TitleAndMetas'
 import Layout from '../components/Layout'
+import HTMLContent from '../components/Shared/Content'
+import ImageThumb from 'gatsby-image'
 
-const AboutPage = () => {
+const ArticleText = styled(HTMLContent)`
+`
+const thumbNail = styled.div`
+  height: 100px;
+  width: 100px;
+`
+
+const AboutPage = ({ data }) => {
   const page = "sub";
-  // const pageData =""
+  const pageData = data.sanityAbout
 
   return (
     <Layout pageLayout={page}>
       <TitleAndMetas
-        metaTitle="About Page"
-        metaDescription="Some description for the about page"
-        metaKeywords="Some keywords for about page"
+        metaTitle={pageData.metaTitle}
+        metaDescription={pageData.metaDescription}
+        metaKeywords={pageData.metaTags}
       />
       <header>
-        <h1>About page</h1>
+        <ArticleText content={pageData._rawTitle} />
       </header>
+      <main>
+        <ArticleText content={pageData._rawProfessionIntro} />
+        <thumbNail>
+          {
+            pageData.mainImage ? (
+              <ImageThumb fluid={pageData.mainImage.asset.fluid} />
+            ) : ''
+          }
+        </thumbNail>
+        <ArticleText content={pageData._rawBody} />
+        <thumbNail>
+          {
+            pageData.backupImage ? (
+              <ImageThumb fluid={pageData.backupImage.asset.fluid} />
+            ) : ''
+          }
+        </thumbNail>
+      </main>
     </Layout>
   )
 }
 
 export default AboutPage;
+
+export const query = graphql`
+
+  query MyQuery {
+    sanityAbout {
+      metaTitle
+      metaTags
+      metaDescription
+      metaTags
+      _rawTitle
+      _rawProfessionIntro
+      mainImage {
+        asset {
+          fluid(maxWidth: 950) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      _rawBody
+      backupImage {
+        asset {
+          fluid(maxWidth: 950) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+    }
+  }
+`;
