06a4db72035d12c6e3e471e9f1f1c9dabb88becb
danielvanc <email@danielvanc.com>
2020-03-19 08:58:11 +0000

    Remove previous bg notification.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,47 +1,56 @@
 /* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled from 'styled-components';
-import BackgroundNotification from './src/components/Specifics/BackgroundNotification';
+// import styled from 'styled-components';
+// import BackgroundNotification from './src/components/Specifics/BackgroundNotification';
 
-const NotifyContain = styled.div`
-  max-width: 1150px;
-  margin: 0 auto;
-  padding: 20px 25px;
-  @media screen and (min-width: 600px) {
-    display: flex;
-  }
-  button {
-    border-radius: 12px;
-    padding: 6px 7px 6px;
-    background: yellow;
-    border: 0;
-    margin-top: 10px;
-    cursor: pointer;
-    @media screen and (min-width: 600px) {
-      margin-left: 2%;
-      margin-top: 0;
-    }
-  }
-`
+// const NotifyContain = styled.div`
+//   max-width: 1150px;
+//   margin: 0 auto;
+//   padding: 20px 25px;
+//   @media screen and (min-width: 600px) {
+//     display: flex;
+//   }
+//   button {
+//     border-radius: 12px;
+//     padding: 6px 7px 6px;
+//     background: yellow;
+//     border: 0;
+//     margin-top: 10px;
+//     cursor: pointer;
+//     @media screen and (min-width: 600px) {
+//       margin-left: 2%;
+//       margin-top: 0;
+//     }
+//   }
+// `
 
 function onServiceWorkerUpdateReady() {
-    const root = document.createElement('div')
-    document.body.appendChild(root)
-  
-    function SWNotify() {
-      return (
-        <>
-          <BackgroundNotification>
-            <NotifyContain>
-              <p>This website has been updated in the background.</p>
-              <button onClick={() => window.location.reload()}>Get the latest version.</button>
-            </NotifyContain>
-          </BackgroundNotification>
-        </>
-      )
+
+    const answer = window.confirm(
+      `This application has been updated. ` +
+      `Reload to display the latest version?`
+    )
+    if (answer === true) {
+      window.location.reload()
     }
-    ReactDOM.render(<SWNotify />, root);
+
+    // const root = document.createElement('div')
+    // document.body.appendChild(root)
+  
+    // function SWNotify() {
+    //   return (
+    //     <>
+    //       <BackgroundNotification>
+    //         <NotifyContain>
+    //           <p>This website has been updated in the background.</p>
+    //           <button onClick={() => window.location.reload()}>Get the latest version.</button>
+    //         </NotifyContain>
+    //       </BackgroundNotification>
+    //     </>
+    //   )
+    // }
+    // ReactDOM.render(<SWNotify />, root);
 }
 
 export {onServiceWorkerUpdateReady};
