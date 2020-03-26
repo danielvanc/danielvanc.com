30408b0e75a61e3151380964d7cea22640dc6fb5
danielvanc <email@danielvanc.com>
2020-03-26 20:09:16 +0000

    [#018] Add pagination to notes overview page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,37 +1,38 @@
-const {createFilePath} = require('gatsby-source-filesystem');
+const { createFilePath } = require('gatsby-source-filesystem');
+const path = require("path");
 
-exports.onCreateWebpackConfig = ({stage, loaders, actions}) => {
-    if (stage === 'build-html') {
-        actions.setWebpackConfig({
-            module: {
-                rules: [
-                    {
-                        test: /github-buttons/,
-                        use: loaders.null(),
-                    },
-                ],
-            },
-        });
-    }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /github-buttons/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
 };
 
-exports.onCreateNode = ({node, getNode, actions}) => {
-    if (node.internal.type === `MarkdownRemark`) {
-        const fileNode = getNode(node.parent);
-        const {createNodeField} = actions;
-        const slug = createFilePath({node, getNode, basePath: `pages`});
-        createNodeField({
-            node,
-            name: `slug`,
-            value: slug,
-        });
-    }
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent);
+    const { createNodeField } = actions;
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
 };
 
 async function createNowPage(graphql, actions, reporter) {
-    const {createPage} = actions;
+  const { createPage } = actions;
 
-    const result = await graphql(`
+  const result = await graphql(`
         {
             allSanityNow(filter: {url: {current: {ne: null}}}) {
                 edges {
@@ -51,90 +52,30 @@ async function createNowPage(graphql, actions, reporter) {
         }
     `);
 
-    if (result.errors) throw result.errors;
+  if (result.errors) throw result.errors;
 
-    const nowPages = (result.data.allSanityNow || {}).edges || [];
-    nowPages.forEach((edge, index) => {
-        const {id, url = {}, publishedAt} = edge.node;
-        const path = `/${url.current}`;
+  const nowPages = (result.data.allSanityNow || {}).edges || [];
+  nowPages.forEach((edge, index) => {
+    const { id, url = {}, publishedAt } = edge.node;
+    const path = `/${url.current}`;
 
-        reporter.info(`Creating Now page: ${path}`);
+    reporter.info(`Creating Now page: ${path}`);
 
-        const templateKey = String(edge.node.template_key.title);
-        createPage({
-            path,
-            component: require.resolve(`./src/templates/${templateKey}.js`),
-            context: {id},
-        });
+    const templateKey = String(edge.node.template_key.title);
+    createPage({
+      path,
+      component: require.resolve(`./src/templates/${templateKey}.js`),
+      context: { id },
     });
+  });
 }
 
-async function createNotePages(graphql, actions, reporter) {
-    const {createPage, createRedirect} = actions;
-    createRedirect({
-        fromPath: `/notes/tags`,
-        isPermanent: true,
-        redirectInBrowser: true,
-        toPath: `/notes`,
-    });
-    createRedirect({
-        fromPath: `/notes/tags/`,
-        isPermanent: true,
-        redirectInBrowser: true,
-        toPath: `/notes`,
-    });
 
-    const result = await graphql(`
-        {
-            allSanityNote(filter: {slug: {current: {ne: null}}}) {
-                edges {
-                    node {
-                        id
-                        slug {
-                            current
-                        }
-                        title
-                        tags {
-                            title
-                        }
-                        _createdAt(formatString: "DD.MM.YYYY")
-                        slug {
-                            current
-                        }
-                        template_key {
-                            title
-                        }
-                        content_type {
-                            title
-                        }
-                    }
-                }
-            }
-        }
-    `);
-
-    if (result.errors) throw result.errors;
-
-    const notes = (result.data.allSanityNote || {}).edges || [];
-    notes.forEach((edge, index) => {
-        const {id, slug = {}, publishedAt} = edge.node;
-        const path = `/notes/${slug.current}`;
-
-        reporter.info(`Creating Notes page: ${path}`);
-
-        const templateKey = String(edge.node.template_key.title);
-        createPage({
-            path,
-            component: require.resolve(`./src/templates/${templateKey}.js`),
-            context: {id},
-        });
-    });
-}
 
 async function createLogPages(graphql, actions, reporter) {
-    const {createPage} = actions;
+  const { createPage } = actions;
 
-    const result = await graphql(`
+  const result = await graphql(`
         {
             allSanityLog(filter: {url: {current: {ne: null}}}) {
                 edges {
@@ -154,26 +95,90 @@ async function createLogPages(graphql, actions, reporter) {
         }
     `);
 
-    if (result.errors) throw result.errors;
+  if (result.errors) throw result.errors;
 
-    const logs = (result.data.allSanityLog || {}).edges || [];
-    logs.forEach((edge, index) => {
-        const {id, url = {}, publishedAt} = edge.node;
-        const path = `/log/${url.current}`;
+  const logs = (result.data.allSanityLog || {}).edges || [];
+  logs.forEach((edge, index) => {
+    const { id, url = {}, publishedAt } = edge.node;
+    const path = `/log/${url.current}`;
 
-        reporter.info(`Creating Log page: ${path}`);
+    reporter.info(`Creating Log page: ${path}`);
 
-        const templateKey = String(edge.node.template_key.title);
-        createPage({
-            path,
-            component: require.resolve(`./src/templates/${templateKey}.js`),
-            context: {id},
-        });
+    const templateKey = String(edge.node.template_key.title);
+    createPage({
+      path,
+      component: require.resolve(`./src/templates/${templateKey}.js`),
+      context: { id },
     });
+  });
 }
 
-exports.createPages = async ({graphql, actions, reporter}) => {
-    await createNotePages(graphql, actions, reporter);
-    await createNowPage(graphql, actions, reporter);
-    await createLogPages(graphql, actions, reporter);
+async function createNotePages(graphql, actions, reporter) {
+  const { createPage, createRedirect } = actions;
+
+  createRedirect({
+    fromPath: `/notes/tags`,
+    isPermanent: true,
+    redirectInBrowser: true,
+    toPath: `/notes`,
+  });
+  createRedirect({
+    fromPath: `/notes/tags/`,
+    isPermanent: true,
+    redirectInBrowser: true,
+    toPath: `/notes`,
+  });
+
+  const result = await graphql(`
+      {
+          allSanityNote(filter: {slug: {current: {ne: null}}}) {
+              edges {
+                  node {
+                      id
+                      slug {
+                          current
+                      }
+                      title
+                      tags {
+                          title
+                      }
+                      _createdAt(formatString: "DD.MM.YYYY")
+                      slug {
+                          current
+                      }
+                      template_key {
+                          title
+                      }
+                      content_type {
+                          title
+                      }
+                  }
+              }
+          }
+      }
+  `);
+  
+  const notes = result.data.allSanityNote.edges;
+  const postsPerPage = 6;
+  const numPages = Math.ceil(notes.length / postsPerPage);
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? '/notes/' : `/notes/${i + 1}`,
+      component: path.resolve("./src/templates/notes-list-template.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    });
+  });
+}
+
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  await createNotePages(graphql, actions, reporter);
+  await createNowPage(graphql, actions, reporter);
+  await createLogPages(graphql, actions, reporter);
 };
