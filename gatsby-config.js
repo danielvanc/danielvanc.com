36c5d6ff7ee0e7bf53db717b9c2c94bb9b58d69f
danielvanc <email@danielvanc.com>
2020-04-26 20:04:26 +0100

    [#038] feat: Add prettify source for sourve view

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,26 @@
 const {
-    api: {projectId, dataset},
-} = require('./studio/sanity.json');
+  api: { projectId, dataset },
+} = require("./studio/sanity.json");
 
-require('dotenv').config({
-    path: `.env`,
+require("dotenv").config({
+  path: `.env`,
 });
 
 module.exports = {
-    siteMetadata: {
-        title: 'Home of Daniel Van Cuylenburg - Guitarist | Front-end Developer',
-        description: "Website of Daniel Van Cuylenburg",
-        siteUrl: "https://www.danielvanc.com/"
-    },
-    plugins: [
-        'gatsby-plugin-styled-components',
-        'gatsby-plugin-react-helmet',
-        'gatsby-plugin-meta-redirect',
-      {
-        resolve: 'gatsby-plugin-feed',
-        options: {
-          query: `
+  siteMetadata: {
+    title: "Home of Daniel Van Cuylenburg - Guitarist | Front-end Developer",
+    description: "Website of Daniel Van Cuylenburg",
+    siteUrl: "https://www.danielvanc.com/",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-meta-redirect",
+    "gatsby-plugin-prettier-build",
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        query: `
           {
             site {
               siteMetadata {
@@ -30,21 +31,21 @@ module.exports = {
             }
           }
         `,
-          feeds: [
-            {
-              serialize: ({ query: { allNotes } }) => {
-                return allNotes.edges.map(({ node: note }) => {
-                  return {
-                    title: note.title,
-                    description: note.description,
-                    url: `https://www.danielvanc.com/notes/${note.slug.current}`,
-                    link: `https://www.danielvanc.com/notes/${note.slug.current}`,
-                    guid: `https://www.danielvanc.com/notes/${note.slug.current}`,
-                    date: note.publishedAt
-                  }
-                })
-              },
-              query: `
+        feeds: [
+          {
+            serialize: ({ query: { allNotes } }) => {
+              return allNotes.edges.map(({ node: note }) => {
+                return {
+                  title: note.title,
+                  description: note.description,
+                  url: `https://www.danielvanc.com/notes/${note.slug.current}`,
+                  link: `https://www.danielvanc.com/notes/${note.slug.current}`,
+                  guid: `https://www.danielvanc.com/notes/${note.slug.current}`,
+                  date: note.publishedAt,
+                };
+              });
+            },
+            query: `
               {
                 allNotes: allSanityNote(
                     sort: {fields: [publishedAt], order: DESC}
@@ -67,70 +68,70 @@ module.exports = {
                   
                 
               }`,
-              output: "/rss.xml",
-              title: "Latest notes from Daniel Van Cuylenburg"
-            }
-          ]
-        }
+            output: "/rss.xml",
+            title: "Latest notes from Daniel Van Cuylenburg",
+          },
+        ],
       },
-        {
-            resolve: `gatsby-source-filesystem`,
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+        ignore: [`**/\.*`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "danielvanc-v1-manifest",
+        short_name: "danielvanc",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "favicon.png", // This path is relative to the root of the site.
+      },
+    },
+    "gatsby-transformer-remark",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-styled-components",
+    "gatsby-transformer-sharp",
+    "react-media",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
             options: {
-                name: `src`,
-                path: `${__dirname}/src/`,
-                ignore: [`**/\.*`],
+              maxWidth: 590,
+              quality: 80,
             },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: 'danielvanc-v1-manifest',
-                short_name: 'danielvanc',
-                start_url: '/',
-                background_color: '#ffffff',
-                theme_color: '#663399',
-                display: 'minimal-ui',
-                icon: 'favicon.png', // This path is relative to the root of the site.
-            },
-        },
-        'gatsby-transformer-remark',
-        'gatsby-plugin-offline',
-        'gatsby-plugin-styled-components',
-        'gatsby-transformer-sharp',
-        'react-media',
-        'gatsby-plugin-sharp',
-        {
-            resolve: 'gatsby-transformer-remark',
-            options: {
-                plugins: [
-                    {
-                        resolve: 'gatsby-remark-images',
-                        options: {
-                            maxWidth: 590,
-                            quality: 80,
-                        },
-                    },
-                ],
-            },
-        },
-        {
-            resolve: 'gatsby-source-sanity',
-            options: {
-                projectId: process.env.PROJECT_ID,
-                dataset: process.env.DATA_SET,
-                token: process.env.SANITY_TOKEN,
-                watchMode: true,
-                overlayDrafts: true,
-            },
-        },
-        // {
-        //     resolve: 'gatsby-plugin-webpack-bundle-analyzer',
-        //     options: {
-        //         production: true,
-        //         disable: !process.env.ANALYZE_BUNDLE_SIZE,
-        //         generateStatsFile: true,
-        //         analyzerMode: 'static',
-        //     },
-        // },
-    ],
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: process.env.PROJECT_ID,
+        dataset: process.env.DATA_SET,
+        token: process.env.SANITY_TOKEN,
+        watchMode: true,
+        overlayDrafts: true,
+      },
+    },
+    // {
+    //     resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+    //     options: {
+    //         production: true,
+    //         disable: !process.env.ANALYZE_BUNDLE_SIZE,
+    //         generateStatsFile: true,
+    //         analyzerMode: 'static',
+    //     },
+    // },
+  ],
 };
