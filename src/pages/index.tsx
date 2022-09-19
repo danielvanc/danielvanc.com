4783f4ffc83296f627719e0d9b1f7790b61d8563
Daniel Van Cuylenburg <email@danielvanc.com>
2022-09-19 15:49:36 +0100

    fix: fixed missing social links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -90,19 +90,10 @@ function Article({ article }: { article: Article }) {
   )
 }
 
-function SocialLink({
-  icon,
-  ...props
-}: {
-  href: string
-  'aria-label': string
-  icon: (props: any) => JSX.Element
-}) {
+function SocialLink({ icon: Icon, ...props }) {
   return (
-    <Link className="group -m-1 p-1" {...props}>
-      {/* TODO: Fix TS Error */}
-      {/* @ts-ignore */}
-      <icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+    <Link href={``} className="group -m-1 p-1" {...props}>
+      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
     </Link>
   )
 }
