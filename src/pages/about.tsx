affde16b4604ba12cb21605d820b7df18ab72f9e
Daniel Van Cuylenburg <email@danielvanc.com>
2022-10-09 15:23:38 +0100

    feat(socials): open in new window

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -31,6 +31,7 @@ function SocialLink({
       <Link
         href={href}
         className="group flex text-sm font-medium text-zinc-800 transition hover:text-pink-500 dark:text-zinc-200 dark:hover:text-pink-500"
+        target="_blank"
       >
         <Icon
           ariaLabel={ariaLabel}
